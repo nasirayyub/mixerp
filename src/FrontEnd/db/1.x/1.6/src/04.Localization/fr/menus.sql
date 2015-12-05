56f3e3369e155d19a6407c94e0a4a9daa5a1c0ef
Nubiancc <nubiancc@hotmail.com>
2015-12-05 16:17:27 +0200

    Update French Translation HRM, Payroll

--- src/FrontEnd/db/1.x/1.6/src/04.Localization/fr/menus.sql
+++ src/FrontEnd/db/1.x/1.6/src/04.Localization/fr/menus.sql
@@ -7,8 +7,10 @@ SELECT * FROM core.create_menu_locale('AS', 'fr', 'Relevé de compte');--Account
 SELECT * FROM core.create_menu_locale('SAT', 'fr', 'Outils d''administration');--Admin Tools
 SELECT * FROM core.create_menu_locale('AGS', 'fr', 'Vieillissement des dalles');--Ageing Slabs
 SELECT * FROM core.create_menu_locale('ALLITMS', 'fr', 'Tous les articles');--All Items
-SELECT * FROM core.create_menu_locale('SAA', 'fr', 'API Stratégie d''accès');--Api Access Policy
 SELECT * FROM core.create_menu_locale('OTSAP', 'fr', 'Paramètres de rattachement');--Attachment Parameters
+SELECT * FROM core.create_menu_locale('ATTNDCE', 'fr', 'Fréquentation');--Attendance
+SELECT * FROM core.create_menu_locale('HRMRPTAT', 'fr', 'Présences');--Attendances
+SELECT * FROM core.create_menu_locale('ATTR', 'fr', 'Attributs');--Attributes
 SELECT * FROM core.create_menu_locale('SEAR', 'fr', 'Rapports d''audit');--Audit Reports
 SELECT * FROM core.create_menu_locale('SAV', 'fr', 'Politique sur la vérification automatique');--Automatic Verification Policy
 SELECT * FROM core.create_menu_locale('BO', 'fr', 'Back-Office');--Back Office
@@ -29,6 +31,7 @@ SELECT * FROM core.create_menu_locale('UPD', 'fr', 'Vérifiez Mise à jour');--C
 SELECT * FROM core.create_menu_locale('SSCD', 'fr', 'Détails de l''élément composé');--Compound Item Details
 SELECT * FROM core.create_menu_locale('SSC', 'fr', 'Composé d''éléments');--Compound Items
 SELECT * FROM core.create_menu_locale('CUOM', 'fr', 'Composé d''unités de mesure');--Compound Units of Measure
+SELECT * FROM core.create_menu_locale('CTRCT', 'fr', 'Contrats');--Contracts
 SELECT * FROM core.create_menu_locale('CC', 'fr', 'Centres de coûts');--Cost Centers
 SELECT * FROM core.create_menu_locale('ICP', 'fr', 'Prix de revient');--Cost Prices
 SELECT * FROM core.create_menu_locale('SCS', 'fr', 'Installation de compteur');--Counter Setup
@@ -40,42 +43,65 @@ SELECT * FROM core.create_menu_locale('OTSCLP', 'fr', 'Currencylayer Paramètres
 SELECT * FROM core.create_menu_locale('CUF', 'fr', 'Les champs personnalisés');--Custom Fields
 SELECT * FROM core.create_menu_locale('OTSDBP', 'fr', 'Paramètres de la base');--Database Parameters
 SELECT * FROM core.create_menu_locale('DBSTAT', 'fr', 'Outil de requête SQL');--Database Statistics
+SELECT * FROM core.create_menu_locale('DEFEAPOL', 'fr', 'Stratégie d''accès par défaut entité');--Default Entity Access Policy
 SELECT * FROM core.create_menu_locale('SDS', 'fr', 'Département installation');--Department Setup
 SELECT * FROM core.create_menu_locale('DRP', 'fr', 'Achat direct');--Direct Purchase
 SELECT * FROM core.create_menu_locale('DRS', 'fr', 'Ventes directes');--Direct Sales
+SELECT * FROM core.create_menu_locale('EDULVL', 'fr', 'Niveaux d''enseignement');--Education Levels
+SELECT * FROM core.create_menu_locale('EMPTYP', 'fr', 'Types d''employés');--Employee Types
+SELECT * FROM core.create_menu_locale('EMPL', 'fr', 'Employés');--Employees
+SELECT * FROM core.create_menu_locale('HRM', 'fr', 'Employés');--Employees
+SELECT * FROM core.create_menu_locale('EMPSTA', 'fr', 'Statuts d''emploi');--Employment Statuses
 SELECT * FROM core.create_menu_locale('EOD', 'fr', 'Fin de l''opération de la journée');--End of Day Operation
+SELECT * FROM core.create_menu_locale('EAPOL', 'fr', 'Politique d''accès de l''entité');--Entity Access Policy
 SELECT * FROM core.create_menu_locale('SES', 'fr', 'Configuration de l''entité');--Entity Setup
 SELECT * FROM core.create_menu_locale('ERR', 'fr', 'Taux d''échange');--Exchange Rates
+SELECT * FROM core.create_menu_locale('EXITTYP', 'fr', 'Types de sortie');--Exit Types
+SELECT * FROM core.create_menu_locale('EXIT', 'fr', 'Sorties');--Exits
 SELECT * FROM core.create_menu_locale('FI', 'fr', 'financement');--Finance
 SELECT * FROM core.create_menu_locale('SFY', 'fr', 'Informations de l''exercice');--Fiscal Year Information
 SELECT * FROM core.create_menu_locale('TRF', 'fr', 'Drapeaux');--Flags
 SELECT * FROM core.create_menu_locale('SFR', 'fr', 'Fréquence & la gestion de l''exercice');--Frequency & Fiscal Year Management
 SELECT * FROM core.create_menu_locale('SAP', 'fr', 'Stratégie d''accès GL');--GL Access Policy
 SELECT * FROM core.create_menu_locale('GRN', 'fr', 'Entrée GRN');--GRN Entry
+SELECT * FROM core.create_menu_locale('GENDR', 'fr', 'Sexes');--Genders
+SELECT * FROM core.create_menu_locale('HRMRPT', 'fr', 'Rapports de gestion des ressources humaines');--HRM Reports
+SELECT * FROM core.create_menu_locale('HOLIDAY', 'fr', 'Installation de vacances');--Holiday Setup
+SELECT * FROM core.create_menu_locale('IDNTYP', 'fr', 'Types d''identification');--Identification Types
 SELECT * FROM core.create_menu_locale('SIS', 'fr', 'Installation de l''industrie');--Industry Setup
+SELECT * FROM core.create_menu_locale('ITM', 'fr', 'Produits et Articles');--Inventory
 SELECT * FROM core.create_menu_locale('IAS', 'fr', 'Relevé de compte de l''inventaire');--Inventory Account Statement
 SELECT * FROM core.create_menu_locale('IIM', 'fr', 'Mouvements de stock');--Inventory Movements
 SELECT * FROM core.create_menu_locale('SIG', 'fr', 'Groupes d''articles');--Item Groups
 SELECT * FROM core.create_menu_locale('SSI', 'fr', 'Gestion des Articles');--Item Maintenance
 SELECT * FROM core.create_menu_locale('SIT', 'fr', 'Types d''éléments');--Item Types
+SELECT * FROM core.create_menu_locale('ITMVAR', 'fr', 'Variantes de l''article');--Item Variants
+SELECT * FROM core.create_menu_locale('JOBTA', 'fr', 'Appellations d''emploi');--Job Titles
 SELECT * FROM core.create_menu_locale('JVN', 'fr', 'Bon écriture');--Journal Voucher Entry
 SELECT * FROM core.create_menu_locale('LF', 'fr', 'Frais de retard');--Late Fees
+SELECT * FROM core.create_menu_locale('LEVAPP', 'fr', 'Quitter l''Application');--Leave Application
+SELECT * FROM core.create_menu_locale('LEVBEN', 'fr', 'Prestations de congé');--Leave Benefits
+SELECT * FROM core.create_menu_locale('LEVTYP', 'fr', 'Types de congé');--Leave Types
 SELECT * FROM core.create_menu_locale('SEAR-LV', 'fr', 'Vue de l''ouverture de session');--Login View
+SELECT * FROM core.create_menu_locale('MATSTS', 'fr', 'Statuts matrimoniaux');--Marital Statuses
 SELECT * FROM core.create_menu_locale('SMA', 'fr', 'Stratégie d''accès menu');--Menu Access Policy
 SELECT * FROM core.create_menu_locale('MFS', 'fr', 'Configuration de frais de Merchant');--Merchant Fee Setup
 SELECT * FROM core.create_menu_locale('SMP', 'fr', 'Divers paramètres');--Miscellaneous Parameters
 SELECT * FROM core.create_menu_locale('OTSMIX', 'fr', 'MixERP Paramètres');--MixERP Parameters
+SELECT * FROM core.create_menu_locale('NTNALY', 'fr', 'Nationalités');--Nationalities
 SELECT * FROM core.create_menu_locale('SOB', 'fr', 'Bureau & de la direction générale de la configuration');--Office & Branch Setup
+SELECT * FROM core.create_menu_locale('OFFHRS', 'fr', 'Heures de bureau');--Office Hours
 SELECT * FROM core.create_menu_locale('SOS', 'fr', 'Installation de Office');--Office Setup
 SELECT * FROM core.create_menu_locale('OTS', 'fr', 'Un réglage de l''heure');--One Time Setup
 SELECT * FROM core.create_menu_locale('OTSOER', 'fr', 'OpenExchangeRates Paramètres');--OpenExchangeRates Parameters
 SELECT * FROM core.create_menu_locale('OTSI', 'fr', 'Stock d''ouverture');--Opening Inventory
+SELECT * FROM core.create_menu_locale('OTHR', 'fr', 'Autre montage');--Other Setup
 SELECT * FROM core.create_menu_locale('PA', 'fr', 'Comptes de tiers');--Party Accounts
 SELECT * FROM core.create_menu_locale('PT', 'fr', 'Types de partie');--Party Types
+SELECT * FROM core.create_menu_locale('PATGR', 'fr', 'Payer des Grades');--Pay Grades
 SELECT * FROM core.create_menu_locale('PAC', 'fr', 'Cartes de paiement');--Payment Cards
 SELECT * FROM core.create_menu_locale('PAT', 'fr', 'Conditions de paiement');--Payment Terms
 SELECT * FROM core.create_menu_locale('SPM', 'fr', 'Gestion des stratégies de');--Policy Management
-SELECT * FROM core.create_menu_locale('ITM', 'fr', 'Produits et Articles');--Products & Items
 SELECT * FROM core.create_menu_locale('PLA', 'fr', 'Profit & compte de la perte');--Profit & Loss Account
 SELECT * FROM core.create_menu_locale('PU', 'fr', 'achat');--Purchase
 SELECT * FROM core.create_menu_locale('PUQ', 'fr', 'Achat & citation');--Purchase & Quotation
@@ -87,8 +113,9 @@ SELECT * FROM core.create_menu_locale('RFC', 'fr', 'Les recettes provenant de cl
 SELECT * FROM core.create_menu_locale('RIS', 'fr', 'Paramètres des factures récurrentes');--Recurring Invoice Setup
 SELECT * FROM core.create_menu_locale('RI', 'fr', 'Factures récurrentes');--Recurring Invoices
 SELECT * FROM core.create_menu_locale('RW', 'fr', 'Report Writer');--Report Writer
-SELECT * FROM core.create_menu_locale('FIR', 'fr', 'Rapports');--Reports
 SELECT * FROM core.create_menu_locale('IR', 'fr', 'Rapports');--Reports
+SELECT * FROM core.create_menu_locale('FIR', 'fr', 'Rapports');--Reports
+SELECT * FROM core.create_menu_locale('RESIGN', 'fr', 'Démissions');--Resignations
 SELECT * FROM core.create_menu_locale('RET', 'fr', 'Des Bénéfices Non Répartis');--Retained Earnings Statement
 SELECT * FROM core.create_menu_locale('SRM', 'fr', 'Gestion des rôles');--Role Management
 SELECT * FROM core.create_menu_locale('OTSSMTP', 'fr', 'Paramètres SMTP');--SMTP Parameters
@@ -108,12 +135,13 @@ SELECT * FROM core.create_menu_locale('SST', 'fr', 'Équipes de vente');--Sales 
 SELECT * FROM core.create_menu_locale('SAR-SBO', 'fr', 'Ventes de Bureau');--Sales by Office
 SELECT * FROM core.create_menu_locale('SPR', 'fr', 'Rapport Salesperson ');--Salesperson Report
 SELECT * FROM core.create_menu_locale('SSA', 'fr', 'Vendeurs/vendeuses');--Salespersons
-SELECT * FROM core.create_menu_locale('OTSSFP', 'fr', 'ScrudFactory Paramètres');--ScrudFactory Parameters
 SELECT * FROM core.create_menu_locale('ISP', 'fr', 'Prix de vente');--Selling Prices
 SELECT * FROM core.create_menu_locale('SET', 'fr', 'Paramètres');--Settings
 SELECT * FROM core.create_menu_locale('SSM', 'fr', 'Installation & entretien');--Setup & Maintenance
+SELECT * FROM core.create_menu_locale('HRMSSM', 'fr', 'Le programme d''installation & entretien');--Setup & Maintenance
 SELECT * FROM core.create_menu_locale('ISM', 'fr', 'Le programme d''installation & entretien');--Setup & Maintenance
 SELECT * FROM core.create_menu_locale('FSM', 'fr', 'Le programme d''installation & entretien');--Setup & Maintenance
+SELECT * FROM core.create_menu_locale('SHIFT', 'fr', 'Quarts de travail');--Shifts
 SELECT * FROM core.create_menu_locale('SHI', 'fr', 'Informations de l''expéditeur');--Shipper Information
 SELECT * FROM core.create_menu_locale('PSA', 'fr', 'Adresses d''expédition');--Shipping Addresses
 SELECT * FROM core.create_menu_locale('STST', 'fr', 'État des Taxes de vente');--State Sales Taxes
@@ -128,10 +156,12 @@ SELECT * FROM core.create_menu_locale('SSP', 'fr', 'Politique de boutique');--St
 SELECT * FROM core.create_menu_locale('STT', 'fr', 'Types de magasins');--Store Types
 SELECT * FROM core.create_menu_locale('STO', 'fr', 'Magasins');--Stores
 SELECT * FROM core.create_menu_locale('OTSSW', 'fr', 'Commutateurs');--Switches
+SELECT * FROM core.create_menu_locale('HRMTA', 'fr', 'Tâches');--Tasks
 SELECT * FROM core.create_menu_locale('TXA', 'fr', 'Administration fiscale');--Tax Authorities
 SELECT * FROM core.create_menu_locale('BOTC', 'fr', 'Configuration de l''impôt');--Tax Configuration
 SELECT * FROM core.create_menu_locale('TXEXT', 'fr', 'Types exonérés de taxe');--Tax Exempt Types
 SELECT * FROM core.create_menu_locale('TXM', 'fr', 'Maître de l''impôt');--Tax Master
+SELECT * FROM core.create_menu_locale('TERMIN', 'fr', 'Terminaisons');--Terminations
 SELECT * FROM core.create_menu_locale('SAR-TSI', 'fr', 'Haut de la page points de vente');--Top Selling Items
 SELECT * FROM core.create_menu_locale('TOPSUP', 'fr', 'supérieur fournisseurs');--Top Suppliers
 SELECT * FROM core.create_menu_locale('FTT', 'fr', 'Modèles de & de transactions');--Transactions & Templates
@@ -139,5 +169,12 @@ SELECT * FROM core.create_menu_locale('TB', 'fr', 'Balance de vérification');--
 SELECT * FROM core.create_menu_locale('UOM', 'fr', 'Unités de mesure');--Units of Measure
 SELECT * FROM core.create_menu_locale('UER', 'fr', 'Mise à jour des taux de change');--Update Exchange Rates
 SELECT * FROM core.create_menu_locale('SUM', 'fr', 'Gestion des utilisateurs');--User Management
+SELECT * FROM core.create_menu_locale('VARIANT', 'fr', 'Variantes');--Variants
+SELECT * FROM core.create_menu_locale('HRMVER', 'fr', 'Vérification');--Verification
+SELECT * FROM core.create_menu_locale('VERCTRCT', 'fr', 'Vérifier les contrats');--Verify Contracts
+SELECT * FROM core.create_menu_locale('VEREXIT', 'fr', 'Vérifier les sorties');--Verify Exits
+SELECT * FROM core.create_menu_locale('VERLEVAPP', 'fr', 'Vérifier les demandes d''autorisation');--Verify Leave Applications
+SELECT * FROM core.create_menu_locale('VERRESIGN', 'fr', 'Vérifier les démissions');--Verify Resignations
+SELECT * FROM core.create_menu_locale('VERTERMIN', 'fr', 'Vérifier les terminaisons');--Verify Terminations
 SELECT * FROM core.create_menu_locale('FVV', 'fr', 'Vérification du bon');--Voucher Verification
 SELECT * FROM core.create_menu_locale('SVV', 'fr', 'Politique sur la vérification bon');--Voucher Verification Policy
