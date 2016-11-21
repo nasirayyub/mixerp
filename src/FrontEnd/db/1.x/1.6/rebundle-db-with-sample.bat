31dd2ea14765a96dd9f9278bc8f805d70de52588
binod <binodnp@outlook.com>
2016-11-21 17:17:38 +0545

    Rebundled database.

--- src/FrontEnd/db/1.x/1.6/rebundle-db-with-sample.bat
+++ src/FrontEnd/db/1.x/1.6/rebundle-db-with-sample.bat
@@ -3,7 +3,7 @@
 bundler\MixERP.Net.Utility.SqlBundler.exe ..\..\..\ "db/1.x/1.6" false true
 
 c:\windows\system32\cscript.exe merge-files.vbs mixerp-incremental-sample.sql ..\..\1.x\1.5\mixerp-incremental-sample.sql mixerp.sql
-copy mixerp.sql ..\..\sample-db.sql
+copy mixerp-incremental-sample.sql ..\..\sample-db.sql
 del mixerp.sql
 
 pause
--- src/FrontEnd/db/1.x/1.6/rebundle-db-without-sample.bat
+++ src/FrontEnd/db/1.x/1.6/rebundle-db-without-sample.bat
@@ -5,7 +5,7 @@ copy mixerp.sql mixerp-patch-for-1.5.sql
 copy mixerp.sql ..\..\patch.sql
 
 c:\windows\system32\cscript.exe merge-files.vbs mixerp-incremental-blank-db.sql ..\..\1.x\1.5\mixerp-incremental-blank-db.sql mixerp.sql
-copy mixerp.sql ..\..\blank-db.sql
+copy mixerp-incremental-blank-db.sql ..\..\blank-db.sql
 del mixerp.sql
 
 pause	
